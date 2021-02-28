21db2ed5a06b747bb61cb9eaa304d693db488137
acmater <adam.mater@anu.edu.au>
2021-03-01 10:44:11 +1100

    Added numrings property and offloaded most template code into the abstract property.py class to make the property API as minimal as possible

--- molsim/properties/numrings.py
+++ molsim/properties/numrings.py
@@ -2,27 +2,25 @@ import numpy as np
 from rdkit import Chem
 from property import Property
 
-class Aromatic(Property):
+
+class NumRings(Property):
     """
-    Whether or not a molecular is aromatic
+    How many rings the molecule contains
     """
     def __init__(self,molecules):
         assert isinstance(molecules[0],str) or isinstance(molecules[0],Chem.rdchem.Mol), "Not a supported molecular format"
         if isinstance(molecules[0],str):
-            print("Using")
             molecules = super().convert_mols(molecules)
 
         self.values = self.calc_property(molecules)
 
     @staticmethod
     def calc_property(molecules):
-        return np.array([bool(x.GetAromaticAtoms()) for x in molecules])
-
-
+        return [x.GetRingInfo().NumRings() for x in molecules]
 
 
 if __name__ == "__main__":
     a = "CCNC"
     b = "c1ccccc1"
     mols = list(map(Chem.MolFromSmiles,[a,b]))
-    print(Aromatic(mols).values)
+    print(NumRings(mols).values)
--- molsim/properties/property.py
+++ molsim/properties/property.py
@@ -3,6 +3,7 @@ Abstract Class for individual molecular properties
 """
 from abc import ABC, abstractmethod
 from rdkit import Chem
+import numpy as np
 
 class Property(ABC):
     """
@@ -12,9 +13,14 @@ class Property(ABC):
     def __init__(self):
         pass
 
-    @abstractmethod
     def __str__(self):
-        pass
+        return f"{np.array_repr(self.values)}"
+
+    def __getitem__(self,idx):
+        return self.values[idx]
+
+    def __len__(self):
+        return len(self.values)
 
     @abstractmethod
     def calc_property(self):
