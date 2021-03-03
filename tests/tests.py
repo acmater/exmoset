7194f6ffa7a17c1b559f4df48cb402674a33e539
acmater <adam.mater@anu.edu.au>
2021-03-03 15:27:33 +1100

    Refactored test code to make it much more readable and extendable

--- tests/tests.py
+++ tests/tests.py
@@ -10,32 +10,47 @@ from molsim.molecule import MolProp
 
 test_mols = molecules4
 
+# Is there a better way to do the global portions of the code below? Intuitively I should be able
+# to customize the initialization of each class and provide it as an attribute, but that doesn't seem to work.
+
 class TestSubstructure(unittest.TestCase):
+    global substructures
+    substructures = Substructure(test_mols,substructures=["[NH2]"])
     def test_substructures(self):
-        substructures = Substructure(test_mols,substructures=["[NH2]"])
         assert sum(substructures["[NH2]"]) == 1, "Incorrectly identifying substructures"
 
-
 class TestDatabaseExtraction(unittest.TestCase):
+    global df, sub_df, molprops
+    df     = pd.read_csv("molsim/data/QM9_Data.csv",index_col="SMILES")
+    sub_df = df.loc[test_mols]
+    molprops = MolProp(test_mols,properties=["Dipole Moment","ZPVE"],df=sub_df)
+
     def test_molprop(self):
-        df     = pd.read_csv("molsim/data/QM9_Data.csv",index_col="SMILES")
-        sub_df = df.loc[test_mols]
-        molprops = MolProp(test_mols,properties=["Dipole Moment","ZPVE"],df=sub_df)
         assert len(molprops["Dipole Moment"] == 86), "Molecular Property dictionary is not being properly generated."
+
+    def test_max_ZPVE(self):
         assert max(molprops["ZPVE"] == 0.216981), "Issue in the ZPVE section of the dictionary."
+
+    def test_fake_prop(self):
+        with self.assertRaises(KeyError):
+            molprops["Electronic Spatial Extent"]
 
 class TestContainsAtom(unittest.TestCase):
     global atom_props
     atom_props = ContainsAtom(test_mols,["C","N","O"])
+
     def test_contains_C(self):
         assert atom_props.entropy(atom_props["C"]) == 0, "Entropy calculation for contains C is broken"
     def test_contains_O(self):
-        assert atom_props.entropy(atom_props["O"])
-
+        assert atom_props.entropy(atom_props["O"]) > 0, "Entropy Calculation for contains O is broken"
+    def test_contains_F(self):
+        with self.assertRaises(KeyError):
+            atom_props["F"]
 
 class TestContainsBond(unittest.TestCase):
     global bond_props
     bond_props = ContainsAtom(test_mols,["SINGLE","DOUBLE","TRIPLE"])
+
     def test_contains_SINGLE(self):
         assert bond_props.entropy(bond_props["SINGLE"]) == 0, "Entropy calculation for contains Single bond is broken"
     def test_contains_TRIPLE(self):
