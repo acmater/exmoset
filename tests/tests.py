14912d8f8bd56b1a88ffc7ce307bc4553938e329
acmater <adam.mater@anu.edu.au>
2021-03-03 14:33:32 +1100

    Finally fixed test directory imports and added pre-commit hook to ensure that all tests pass

--- tests/tests.py
+++ tests/tests.py
@@ -7,11 +7,11 @@ print(sys.path)
 
 from molsim.atom import *
 from molsim.bond import *
-from molsim.substructure import *
-from molsim.data import 
+from molsim.substructure import Substructure
+from molsim.data import *
+from molsim.molecule import MolProp
 
 test_mols = molecules4
-
 
 class TestSubstructure(unittest.TestCase):
     def test_substructures(self):
@@ -21,7 +21,7 @@ class TestSubstructure(unittest.TestCase):
 
 class TestDatabaseExtraction(unittest.TestCase):
     def test_molprop(self):
-        df     = pd.read_csv("data/QM9_Data.csv",index_col="SMILES")
+        df     = pd.read_csv("molsim/data/QM9_Data.csv",index_col="SMILES")
         sub_df = df.loc[test_mols]
         molprops = MolProp(test_mols,properties=["Dipole Moment","ZPVE"],df=sub_df)
         assert len(molprops.values["Dipole Moment"] == 86), "Molecular Property dictionary is not being properly generated."
