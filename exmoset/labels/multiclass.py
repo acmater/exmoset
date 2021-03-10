15bb657ab2a7a8d6492367f056879d75a3531fd9
acmater <adam.mater@anu.edu.au>
2021-03-11 09:40:30 +1100

    Updated multiclass label

--- exmoset/labels/multiclass.py
+++ exmoset/labels/multiclass.py
@@ -4,5 +4,19 @@ class Multiclass(Label):
     """
     Class that handles the manipulation of multiclass molecular labels.
     """
-    def __init__(self,values):
-        self.values = values
+    def __init__(self,name,
+                      values,
+                      context='atom'):
+        self.name    = name
+        self.values  = values
+        self.av      = np.round(np.mean(values))
+
+    def summary(self,verbose=False):
+        if self.context == "atom":
+            return f"Atoms share a similar {self.name} centred around {self.av}"
+        elif self.context == "bonds":
+            return f"Bonds share a similar {self.name} centred around {self.av}"
+        elif self.context == "molecule":
+            return f"Molecules share a similar {self.name} centred around {self.av}"
+        else:
+            return f"{self.context} {self.name} at {self.av}"
